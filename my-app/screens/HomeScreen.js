f449143f6c845c391b924668fc13c59b150c1e6e
patriceandrea <andrea_pavico98@yahoo.ca>
2022-11-28 23:42:54 -0500

    adds icons

--- my-app/screens/HomeScreen.js
+++ my-app/screens/HomeScreen.js
@@ -1,13 +1,61 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, SafeAreaView, Image, TextInput } from 'react-native'
+import React, { useLayoutEffect } from 'react'
 import { StatusBar } from 'expo-status-bar';
+import { useNavigation } from '@react-navigation/native'
+import {
+  UserIcon,
+  ChevronDownIcon,
+  SearchIcon
+} from 'react-native-heroicons/outline'
+import { AdjustmentsIcon } from 'react-native-heroicons/outline'
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
+import { faMagnifyingGlass, faSlidersH } from '@fortawesome/free-solid-svg-icons'
+
 
 const HomeScreen = () => {
+  const navigation = useNavigation()
+
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    })
+  }, [])
+
+
   return (
-    <View>
-      <Text className='text-red-500'>Hello World!</Text>
-      <StatusBar style="auto" />
-    </View >
+    <SafeAreaView className='bg-white pt-5' >
+      <Text className='text-red-500'>
+        { }
+        <View className='flex-row pb-3 items-center mx-4 space-x-2 px-2'>
+          <Image
+            source={{
+              uri: 'https://links.papareact.com/wru',
+            }} className='h-7 w-7 bg-gray-300 p-4 rounded-full' />
+          <View className='flex-1'>
+            <Text className='font-bold text-gray-400 text-xs'> Deliverr!</Text>
+            <Text className='font-bold text-xl'> Current Location
+              <ChevronDownIcon size={20} color={"#00CCBB"} />
+              <Text />
+            </Text>
+          </View>
+          <UserIcon size={35} color="#00CCBB" />
+        </View>
+
+
+        <View>
+          <View className='flex-row'>
+            <FontAwesomeIcon icon={faMagnifyingGlass} size={25} color='blue' />
+            <TextInput
+              placeholder='Restaurants and cuisines'
+              keyboardType='default'
+            />
+          </View>
+
+          <FontAwesomeIcon icon={faSlidersH} size={30} color="#00CCBB" />
+        </View>
+      </Text>r
+    </SafeAreaView>
   )
 }
 
