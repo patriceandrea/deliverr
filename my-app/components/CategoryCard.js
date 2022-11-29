2e128a02cb108647db6055fb234aa12be48fce21
patriceandrea <andrea_pavico98@yahoo.ca>
2022-11-29 15:06:07 -0500

    changes the tailwind config

--- my-app/components/CategoryCard.js
+++ my-app/components/CategoryCard.js
@@ -3,11 +3,11 @@ import React from 'react'
 
 const CategoryCard = ({ imgUrl, title }) => {
   return (
-    <TouchableOpacity className='relative mr-2'>
+    <TouchableOpacity className='relative mr-3'>
 
       <Image
         source={{
-          uri: imgUrl, width: 200, height: 200,
+          uri: imgUrl, width: 100, height: 100,
         }}
         className='h-200 w-200 rounded'
       />
