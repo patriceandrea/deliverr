86c1ba22443ec60e1d7095b379ad99f93f75229b
patriceandrea <andrea_pavico98@yahoo.ca>
2022-11-30 11:00:54 -0500

    implements sanity

---
+++ my-app/sanity/schemas/restaurant.js
@@ -0,0 +1,15 @@
+export default {
+  name: 'restaurant',
+  title: 'Restaurant',
+  type: 'document',
+  fields: [
+    {
+      name: "name",
+      type: "string",
+      title: "Restaurant name",
+      validation: (Rule) => Rule.required(),
+    }
+
+  ],
+
+}
