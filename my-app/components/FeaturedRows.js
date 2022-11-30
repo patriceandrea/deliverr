9da20826e5eda9e5484c71ea7d3bd47729c9a335
patriceandrea <andrea_pavico98@yahoo.ca>
2022-11-30 00:50:58 -0500

    adds Restaurant Card component

--- my-app/components/FeaturedRows.js
+++ my-app/components/FeaturedRows.js
@@ -1,10 +1,89 @@
-import { View, Text } from 'react-native'
+import { View, Text, ScrollView } from 'react-native'
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons'
+import RestaurantCard from './RestaurantCard'
 
-const FeaturedRows = ({ title, description, featuredCategory }) => {
+const FeaturedRows = ({ id, title, description, featuredCategory }) => {
   return (
     <View>
-      <Text>{title}: {description}</Text>
+      <View className='mt-4 flex-row items-center justify-between px-4'>
+        <Text className='font-bold text-lg'>{title}</Text>
+        <FontAwesomeIcon icon={faArrowRight} color="#63b5cf" />
+      </View>
+
+      <Text className='text-xs text-gray-500 px-4'>{description}</Text>
+
+      <ScrollView
+        horizontal
+        contentContainerStyle={{
+          paddingHorizontal: 15
+        }}
+        showsHorizontalScrollIndicator={false}
+        className='pt-5'
+      >
+        {/* Restaurants Card */}
+        <RestaurantCard
+          id={123}
+          imgUrl='https://links.papareact.com/gn7'
+          title="Nando's"
+          rating={4.5}
+          genre='South African'
+          address='123 Main St'
+          short_description='This is a short description'
+          dishes={[]}
+          long={20}
+          lat={0}
+        />
+        <RestaurantCard
+          id={123}
+          imgUrl='https://links.papareact.com/gn7'
+          title="Nando's"
+          rating={4.5}
+          genre='South African'
+          address='123 Main St'
+          short_description='This is a short description'
+          dishes={[]}
+          long={20}
+          lat={0}
+        />
+        <RestaurantCard
+          id={123}
+          imgUrl='https://links.papareact.com/gn7'
+          title="Nando's"
+          rating={4.5}
+          genre='South African'
+          address='123 Main St'
+          short_description='This is a short description'
+          dishes={[]}
+          long={20}
+          lat={0}
+        />
+        <RestaurantCard
+          id={123}
+          imgUrl='https://links.papareact.com/gn7'
+          title="Nando's"
+          rating={4.5}
+          genre='South African'
+          address='123 Main St'
+          short_description='This is a short description'
+          dishes={[]}
+          long={20}
+          lat={0}
+        />
+        <RestaurantCard
+          id={123}
+          imgUrl='https://links.papareact.com/gn7'
+          title="Nando's"
+          rating={4.5}
+          genre='South African'
+          address='123 Main St'
+          short_description='This is a short description'
+          dishes={[]}
+          long={20}
+          lat={0}
+        />
+      </ScrollView>
     </View>
   )
 }
--- my-app/screens/HomeScreen.js
+++ my-app/screens/HomeScreen.js
@@ -63,21 +63,24 @@ const HomeScreen = () => {
         <Categories />
         {/*Featured Rows Component*/}
         <FeaturedRows
+          id="123"
           title="Featured"
           description="Paid placements from our partners"
           featuredCategory="featured"
         />
         {/*Featured Rows: Tasty Discount*/}
         <FeaturedRows
+          id="123"
           title="Tasty Discount"
-          description="Paid placements from our partners"
-          featuredCategory="featured"
+          description="Everyone's been enjoying these juicy discounts"
+          featuredCategory="discounts"
         />
         {/*Offers Near You: Tasty Discount*/}
         <FeaturedRows
-          title="Featured"
-          description="Paid placements from our partners"
-          featuredCategory="featured"
+          id="123"
+          title="Offers near you"
+          description="Why not support your local restaurant tonight!"
+          featuredCategory="offers"
         />
       </ScrollView>
     </SafeAreaView>
